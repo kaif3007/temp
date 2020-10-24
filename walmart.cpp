42f5089c3c67eb291fefe83248652ae90b7086fa
Md kaif khan <kaif.17je002751@ee.ism.ac.in>
2020-10-24 09:48:11 +0530

    Update walmart.cpp

--- walmart.cpp
+++ walmart.cpp
@@ -1,3 +1,56 @@
+#########################################################################################################
+https://leetcode.com/discuss/interview-question/684944/walmart-labs-oa-question
+
+string s;
+     cin>>s;
+
+     int n=s.length();
+     int dp[n][n];
+
+     for(int i=0;i<n;i++)
+        dp[i][i]=1;
+
+    for(int l=2;l<=n;l++) 
+    {
+        for(int i=0;i<=n-l;i++)
+         {
+            int j = i+l-1;
+            string substr = s.substr(i,l);
+            
+                dp[i][j]=l;
+               
+                for(int k = i; k<j;k++) 
+                {
+                    if(dp[i][k] + dp[k+1][j]< dp[i][j])
+                        dp[i][j] = dp[i][k] + dp[k+1][j];
+                    
+                }
+                
+                for(int k=1;k<l;k++)
+                {
+                    string repeatStr = substr.substr(0,k);
+                    
+                    if(l%k == 0)
+                    {
+                          string kaif="";
+                          int cnt= l/k;
+                          for(int g=0;g<cnt;g++)
+                            kaif+=repeatStr;
+
+                        if(kaif==substr)
+                        {
+                           int ss = dp[i][i+k-1];
+                          if(ss< dp[i][j]) 
+                            dp[i][j] = ss;
+                        }
+                          
+                    }
+                }
+            }
+        }
+    
+    
+    cout<<dp[0][s.length()-1];
 ############################################################################################################
 FAKE PALINDROME
 
