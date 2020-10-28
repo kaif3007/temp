3ed3e33baa7672d6fd2c41e8abdf01f3b908196a
Md kaif khan <kaif.17je002751@ee.ism.ac.in>
2020-10-28 17:06:21 +0530

    Update walmart.cpp

--- walmart.cpp
+++ walmart.cpp
@@ -1,3 +1,164 @@
+#######################################################################
+CANDY SHOPS
+
+#include "bits/stdc++.h"
+#define ll  long long int
+#define BUCKET_SIZE (int)sqrt(100000*1.0)+1
+#define MAX = 100000;
+#define mod 1e9+7
+
+
+
+using namespace std;
+
+
+class Node
+{
+public:
+    ll wt,val,csum;
+
+     Node(ll wt,ll val)
+      {
+        this->wt = wt;
+        this->val = val;
+        csum = 0;
+    }
+
+};
+
+bool comp(Node* n1,Node* n2)
+{
+    return n1->wt<n2->wt;
+}
+
+ void build_bucket(int i, vector<vector<Node*>> buckets) 
+ {
+
+    sort(buckets[i].begin(),buckets[i].end(),comp);
+
+    for(int j=0;j<buckets[i].size();++j) 
+    {
+        if (j > 0) 
+            buckets[i][j]->csum = buckets[i][j]->val + buckets[i][j-1]->csum;
+        else 
+            buckets[i][j]->csum = buckets[i][j]->val;
+    }
+}
+
+ ll upperBound(vector<Node*> bucket, int low, ll w) 
+ {
+
+    int high=bucket.size();
+    while (low<high){
+        int mid=(low+high)>>1;
+        if(bucket[mid]->wt<=w){
+            low=mid+1;
+        }else{
+            high=mid;
+        }
+    }
+
+    low--;
+    if(low>-1)
+        return bucket[low]->csum;
+    return 0L;
+}
+
+
+ int main()
+ {
+
+    int n,q;
+    cin>>n>>q;
+
+    ll* wt=new ll[n];
+    ll* val=new ll[n];
+    ll* wt_copy=new ll[n];
+
+    for(int i=0;i<n;++i)
+    {
+        cin>>wt[i];
+        wt_copy[i]=wt[i];
+    }
+
+    for(int i=0;i<n;++i)
+        cin>>val[i];
+    
+
+    vector<vector<Node*> > buckets(BUCKET_SIZE);
+
+    for(int i=0;i<n;++i)
+        buckets[i/BUCKET_SIZE].push_back(new Node(wt[i],val[i]));
+    
+
+
+    for(int i=0;i<BUCKET_SIZE;++i)
+        build_bucket(i,buckets);
+    
+    while (q>0)
+    {
+        int t; 
+        cin>>t;
+        if(t==1)
+        {
+            ll ans=0;
+            int l, r;
+            ll w;
+            cin>>l>>r>>w;
+            l-=1;r-=1;
+
+            int RL=l/BUCKET_SIZE,RR=r/BUCKET_SIZE;
+            if(RL==RR)
+            {
+                for(int i=l;i<=r;++i)
+                    if(wt[i]<=w)
+                        ans+=val[i];
+            }
+            else
+            {
+                int end=(RL+1)*BUCKET_SIZE-1;
+                for(int i=l;i<=min(end,n-1);++i)
+                {
+                    if(wt[i]<=w)
+                        ans+=val[i];
+                }
+                for(int i=RL+1;i<RR;++i)
+                    ans+=upperBound(buckets[i],0,w);
+                
+                for(int i=RR*BUCKET_SIZE;i<=r;++i)
+                {
+                    if(wt[i]<=w)
+                        ans+=val[i];
+                }
+            }
+            cout<<ans<<endl;
+        }
+        else
+        {
+            int idx,type;
+            cin>>idx>>type;
+
+            idx-=1;
+
+            int BB=idx/BUCKET_SIZE;
+            int end=(BB+1)*BUCKET_SIZE-1;
+            int start=end-(BUCKET_SIZE-1);
+            if(type==0)
+                wt[idx]=INT_MAX;
+            else
+                wt[idx]=wt_copy[idx];
+            
+
+            for(int i=start;i<=min(end,n-1);++i)
+                buckets[BB].push_back(new Node(wt[i],val[i]));
+            
+            build_bucket(BB,buckets);
+        }
+
+        q--;
+    }
+
+}
 #########################################################################################################
 https://leetcode.com/discuss/interview-question/684944/walmart-labs-oa-question
 
