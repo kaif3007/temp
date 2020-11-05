b1e7c210f456633dfdee53ccc6542df5acb80286
Md kaif khan <kaif.17je002751@ee.ism.ac.in>
2020-11-05 09:02:12 +0530

    Create leetcode-problems.cpp

---
+++ amazon-OA/leetcode-problems.cpp
@@ -0,0 +1,10 @@
+BETA-TESTING:->   https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/submissions/
+CRITICAL CONNECTIONS:->   https://leetcode.com/problems/critical-connections-in-a-network/
+NO OF ISLAND:->  https://leetcode.com/problems/number-of-islands/
+MOST COMMON WORDS:->  https://leetcode.com/problems/most-common-word/
+ROBOTICS CHALLENGE:-> https://leetcode.com/problems/baseball-game/description/
+K CLOSEST POINT:-> https://leetcode.com/problems/k-closest-points-to-origin/
+GIFTING GROUPS:-> https://leetcode.com/problems/friend-circles/
+FILL THE TRUCK:-> https://leetcode.com/discuss/interview-question/918270/
+DISC SPACE UTILIZATION:-> https://leetcode.com/problems/sliding-window-maximum/
+BREAK A PALINDROME:-> https://leetcode.com/problems/break-a-palindrome/
